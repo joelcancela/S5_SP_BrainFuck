6514f4add6fb16abd9ab1db7aae8d2bf23b6b57d
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2016-09-25 21:05:46 +0200

    Test mergetool

--- src/main/java/unice/polytech/polystirN/brainfuck/interpreter/Interpreter.java
+++ src/main/java/unice/polytech/polystirN/brainfuck/interpreter/Interpreter.java
@@ -90,7 +90,7 @@ public abstract class Interpreter {
     /**
      * Set the current memory cell.
      *
-     * @param New value of p.
+     * @param nP is the new value of p.
      */
     public static void setP(int nP) {
         p = nP;
