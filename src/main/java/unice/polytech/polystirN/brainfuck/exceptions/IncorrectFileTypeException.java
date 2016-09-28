2891cf5dffa0e85d93a548087e9ae17b77c7a961
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2016-09-28 17:20:14 +0200

    Added incorrectFileTypeException

---
+++ src/main/java/unice/polytech/polystirN/brainfuck/exceptions/IncorrectFileTypeException.java
@@ -0,0 +1,13 @@
+package unice.polytech.polystirN.brainfuck.exceptions;
+
+/**
+ * Exception class for incorrect file types (differents from *.bf)
+ *
+ * @author Joël CANCELA VAZ and Pierre RAINERO
+ * @author Tanguy INVERNIZZI and Aghiles DZIRI
+ */
+public class IncorrectFileTypeException extends Exception{
+    public IncorrectFileTypeException(String message) {
+        super(message);
+    }
+}
