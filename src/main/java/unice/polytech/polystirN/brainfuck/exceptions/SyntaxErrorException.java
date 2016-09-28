dae281788f95c1a32343a356378078dc5613089c
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2016-09-28 17:17:18 +0200

    Added SyntaxError Exception

---
+++ src/main/java/unice/polytech/polystirN/brainfuck/exceptions/SyntaxErrorException.java
@@ -0,0 +1,14 @@
+package unice.polytech.polystirN.brainfuck.exceptions;
+
+/**
+ * Exception class for syntax errors in files
+ * (positive and negative)
+ *
+ * @author Joël CANCELA VAZ and Pierre RAINERO
+ * @author Tanguy INVERNIZZI and Aghiles DZIRI
+ */
+public class SyntaxErrorException extends Exception {
+    public SyntaxErrorException(String message) {
+        super(message);
+    }
+}
