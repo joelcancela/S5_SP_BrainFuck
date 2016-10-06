7c50bca49d105813777ba2e8aa301f2c1b4a669a
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2016-10-06 16:33:12 +0200

    Updated Javadoc for In and Out. I did also some reformating of the code in both of them.

--- src/main/java/unice/polytech/polystirN/brainfuck/language/In.java
+++ src/main/java/unice/polytech/polystirN/brainfuck/language/In.java
@@ -4,49 +4,51 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 import unice.polytech.polystirN.brainfuck.interpreter.Interpreter;
+
 /**
  * Class used to specify the In operator behaviour
  *
  * @author Joël CANCELA VAZ and Pierre RAINERO
  * @author Tanguy INVERNIZZI and Aghiles DZIRI
  */
-public class In implements Operator{
+public class In implements Operator {
+	
 	private String filename;
 	private Scanner sc;
 	private BufferedReader buffer;
+
 	/**
-	 * constructor
-	 * @param filename if it exist else null
+	 * Constructor of the In class
+	 * @param The filename where In is supposed to read, or null if the operator 
+	 * 		  is supposed to read on the standard input.
 	 */
-	public In(String filename){
+	public In(String filename) {
 		this.filename=filename;
 	}
+
 	/**
-	 * methode doOperation read a charater on the Console or on a file
+	 *  Read a character, either on the standard output or in a file, and put
+	 *  it in the current cell memory as the new value of the cell, erasing
+	 *  it's previous content.
 	 *  @param object of type Interpreter
-	 *  @return boolean
+	 *  @return true if the character was successfully written, false if not.
 	 */
 	@Override
 	public boolean doOperation(Interpreter interpreter) throws Exception {
-		if(filename==null){
+		if(filename == null) {
 			sc = new Scanner(System.in);
 			interpreter.getMemory().getCells()[interpreter.getMemory().getP()]=(byte)sc.next().charAt(0);
 		}
-		else{
-			try{
-		
+		else {
+			try {
 				buffer = new BufferedReader(new FileReader(filename));
 				interpreter.getMemory().getCells()[interpreter.getMemory().getP()]=(byte)buffer.read();
 			}
-			catch(Exception e){
+			catch(Exception e) {
 				System.out.println(e.toString());
 			}
 			buffer.close();
 		}
-		
 		return true;
 	}
-	
-	
-
 }
--- src/main/java/unice/polytech/polystirN/brainfuck/language/Out.java
+++ src/main/java/unice/polytech/polystirN/brainfuck/language/Out.java
@@ -2,8 +2,8 @@ package unice.polytech.polystirN.brainfuck.language;
 
 import java.io.FileWriter;
 import java.io.IOException;
-
 import unice.polytech.polystirN.brainfuck.interpreter.Interpreter;
+
 /**
  * Class used to specify the Out operator behaviour
  *
@@ -11,40 +11,38 @@ import unice.polytech.polystirN.brainfuck.interpreter.Interpreter;
  * @author Tanguy INVERNIZZI and Aghiles DZIRI
  */
 public class Out implements Operator {
+	
 	private String filename;
+
 	/**
-	 * constructor
-	 * @param filename if it exist else null
+	 * Constructor of the Out class
+	 * @param The filename where out is supposed to write, or null if the operator 
+	 * 		  is supposed to write on the standard output.
 	 */
-	public Out(String filename){
+	public Out(String filename) {
 		this.filename=filename;
 	}
+
 	/**
-	 * methode doOperation write a charater on the Console or on a file
+	 *  Write the current cell memory content as an ASCII character, either on the standard output or in a file.
 	 *  @param object of type Interpreter
-	 *  @return boolean
+	 *  @return true if the character was successfully written, false if not.
 	 */
 	@Override
 	public boolean doOperation(Interpreter interpreter) throws Exception {
-		if(filename==null){
+		if(filename==null) {
 			System.out.println((char)(interpreter.getMemory().getCells()[interpreter.getMemory().getP()] & 0xFF));
 		}
 		else {
-			try
-			{
-			    FileWriter fichierw = new FileWriter (filename,true);
-			    fichierw.write(((char)(interpreter.getMemory().getCells()[interpreter.getMemory().getP()] & 0xFF)));
-			    fichierw.close();
+			try {
+				FileWriter fichierw = new FileWriter (filename,true);
+				fichierw.write(((char)(interpreter.getMemory().getCells()[interpreter.getMemory().getP()] & 0xFF)));
+				fichierw.close();
 			}
-			catch (IOException exception)
-			{
-			    System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
-			}
-			
+			catch (IOException exception) {
+				System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
+			}		
 		}
 		return true;
 	}
-	
-	
-	
 }
