4c8957e4bf5b00158809560ab6be393beb692b96
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2016-10-13 17:36:33 +0200

    Javadoc reader

--- src/main/java/unice/polytech/polystirN/brainfuck/interpreter/Reader.java
+++ src/main/java/unice/polytech/polystirN/brainfuck/interpreter/Reader.java
@@ -1,8 +1,14 @@
 package unice.polytech.polystirN.brainfuck.interpreter;
 
+/**
+ * TODO Maybe implement Iterable
+ * Abstract class for the input reader
+ */
 public abstract class Reader {
-	
-	public abstract boolean hasNext() throws Exception;
-	public abstract String next() throws Exception;
-	public abstract Object getBuffer();
+
+    public abstract boolean hasNext() throws Exception;
+
+    public abstract String next() throws Exception;
+
+    public abstract Object getBuffer();
 }
