a6ab66a6e6782d1f365780bad05704fd011fd8b5
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2016-10-20 13:51:01 +0200

    Added current case pointer and value

--- src/main/java/unice/polytech/polystirN/brainfuck/interpreter/Interpreter.java
+++ src/main/java/unice/polytech/polystirN/brainfuck/interpreter/Interpreter.java
@@ -94,6 +94,7 @@ public class Interpreter {
                 op.execute(this);
             }
         }
+        System.out.println("C"+memory.getP()+": "+memory.getCells()[memory.getP()]);
         return true;
     }
 
