034aedc4fa3557ec8b619641fb628f8338f9df36
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2016-10-20 16:34:01 +0200

    Added the script for the demo of the first presentation

---
+++ demo.sh
@@ -0,0 +1,52 @@
+clear
+echo cat examples/L2/JUMP2.bf
+cat examples/L2/JUMP2.bf
+echo
+read
+echo ./bfck -p examples/l2/jump2.bf --rewrite
+./bfck -p examples/L2/jump2.bf --rewrite
+echo
+echo
+
+read
+echo cat examples/L2/JUMP2.bf
+cat examples/L2/JUMP2.bf
+echo
+read
+echo ./bfck -p examples/l2/jump2.bf --check
+./bfck -p examples/L2/jump2.bf --check
+echo
+echo
+
+read
+echo cat examples/L2/bonjour.bf
+cat examples/L2/bonjour.bf
+echo
+read
+rm bonjour
+echo ./bfck -p examples/l2/bonjour.bf -o bonjour
+./bfck -p examples/L2/bonjour.bf -o bonjour
+echo
+read
+echo cat bonjour
+cat bonjour
+
+read
+clear
+read
+echo cat examples/L2/helloworld.bf
+cat examples/L2/helloworld.bf
+echo
+read
+echo ./bfck -p examples/l2/helloworld.bf
+./bfck -p examples/L2/helloworld.bf
+
+echo
+echo
+read
+echo AFFICHAGE DE HELLOWORLD.BMP
+open examples/images/helloworld.bmp
+echo
+read
+echo ./bfck -p examples/images/helloworld.bmp
+./bfck -p examples/images/helloworld.bmp
