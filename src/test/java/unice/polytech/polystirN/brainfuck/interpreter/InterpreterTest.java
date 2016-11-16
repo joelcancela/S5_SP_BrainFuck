cc6272708ca00b728c9a60fbd287bc36812c989c
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2016-11-16 16:04:49 +0100

    Added TU

--- src/test/java/unice/polytech/polystirN/brainfuck/interpreter/InterpreterTest.java
+++ src/test/java/unice/polytech/polystirN/brainfuck/interpreter/InterpreterTest.java
@@ -1,7 +1,5 @@
 package unice.polytech.polystirN.brainfuck.interpreter;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.fail;
@@ -15,13 +13,18 @@ import static org.junit.Assert.assertEquals;
  */
 public class InterpreterTest {
     private Interpreter intrptr;
-    private int mask = 0xff;
+    private int mask = 0xff; //Use to translate bytes into integers
     private String inputMock = getClass().getResource("/L2/mocks/input.txt").getFile();
+
+
+    /**************
+     ** LEVEL 1 **
+     **************/
 
     @Test
     public void interpretBfLongUsualFiles() throws Exception {
 
-        //Simple decrements after increments
+        //Simple 3 increments and 3 decrements
         intrptr = new Interpreter(getClass().getResource("/L1/usual/DECRSimple.bf").getFile());
         intrptr.interpretFile();
         assertEquals(0, intrptr.getMemory().getP());//Pointer hasn't moved
@@ -97,6 +100,10 @@ public class InterpreterTest {
 
     }
 
+    /**************
+     ** LEVEL 2 **
+     **************/
+
     @Test
     public void interpretBfShortUsualFiles() throws Exception {
         //Writes "Bonjour" using loops
@@ -110,26 +117,116 @@ public class InterpreterTest {
         assertEquals(111, intrptr.getMemory().getCells()[4] & mask);//111 is o in ascii
         assertEquals(117, intrptr.getMemory().getCells()[5] & mask);//117 is u in ascii
         assertEquals(114, intrptr.getMemory().getCells()[6] & mask);//114 is r in ascii
-        
-        //Comments
-        intrptr = new Interpreter("./examples/L3/prog_com.bf");
+
+        //Alternate increment and decrements 3 times each
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/decrementSimple.bf").getFile());
         intrptr.interpretFile();
-        assertEquals(0, intrptr.getMemory().getP());
-        assertEquals(1, intrptr.getMemory().getCells()[0] & mask);
-        
-        intrptr = new Interpreter("./examples/L3/prog_com&indente.bf");
+        assertEquals(0, intrptr.getMemory().getP());//Result of this program is C0:0
+        assertEquals(0, intrptr.getMemory().getCells()[0] & mask);//The cell number 0 is at 0
+
+        //Writes "Hello World!" using loops
+        //It uses only 5 memory cells, containing "Hello_" then "World!"
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/helloworld.bf").getFile());
         intrptr.interpretFile();
-        
-        intrptr = new Interpreter("./examples/L3/prog_fullcom.bf");
+        assertEquals(4, intrptr.getMemory().getP());//Result of this program is C4:10
+        assertEquals(0, intrptr.getMemory().getCells()[0] & mask);//0 is a space in ascii
+        assertEquals(87, intrptr.getMemory().getCells()[1] & mask);//87 is W in ascii
+        assertEquals(100, intrptr.getMemory().getCells()[2] & mask);//100 is d in ascii
+        assertEquals(33, intrptr.getMemory().getCells()[3] & mask);//33 is ! in ascii
+        assertEquals(10, intrptr.getMemory().getCells()[4] & mask);//10 is a line feed in ascii
+
+        //In, reads the first char of the input file and write it in a memory cell
+        //Here it's "L"
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/in.bf").getFile(), inputMock, null);
         intrptr.interpretFile();
+        assertEquals(0, intrptr.getMemory().getP());//Result of this program is C0:76
+        assertEquals(76, intrptr.getMemory().getCells()[0] & mask);
+
+        //Increment 7 times
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/incrementC0by7.bf").getFile());
+        intrptr.interpretFile();
+        assertEquals(0, intrptr.getMemory().getP());//Pointer hasn't moved
+        assertEquals(7, intrptr.getMemory().getCells()[0] & mask);//Result of this program is C0:7
+
+        //Increment 255 times
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/incrementMax255.bf").getFile());
+        intrptr.interpretFile();
+        assertEquals(0, intrptr.getMemory().getP());//Pointer hasn't moved
+        assertEquals(255, intrptr.getMemory().getCells()[0] & mask);//Result of this program is C0:255
+
+        //TODO
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/jumpInternalLoop.bf").getFile());
+        intrptr.interpretFile();
+
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/jumpInternalLoop2.bf").getFile());
+        intrptr.interpretFile();
+
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/jumpInternalLoop3.bf").getFile());
+        intrptr.interpretFile();
+
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/jumpSimple.bf").getFile());
+        intrptr.interpretFile();
+
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/jumpSimple2.bf").getFile());
+        intrptr.interpretFile();
+
+        intrptr = new Interpreter(getClass().getResource("/L2/usual/left&right1.bf").getFile());
+        intrptr.interpretFile();
+
+
     }
 
     @Test
     public void interpretBfShortErrorFiles() throws Exception {
-//        //Prints a cat of the inputMock file
-//        intrptr = new Interpreter(getClass().getResource("/L2/errors/cat.bf").getFile(),inputMock,null);
-//        intrptr.interpretFile();
+        intrptr = new Interpreter(getClass().getResource("/L2/errors/decrementC0by7.bf").getFile());
+        try {
+            intrptr.interpretFile();
+            fail();
+        } catch (Exception e) {
+            assertEquals("MemoryUnderflowException", e.getClass().getSimpleName());
+            assertEquals("value can't be negative", e.getMessage());
+        }
+
+
+        intrptr = new Interpreter(getClass().getResource("/L2/errors/decrementError.bf").getFile());
+        try {
+            intrptr.interpretFile();
+            fail();
+        } catch (Exception e) {
+            assertEquals("MemoryUnderflowException", e.getClass().getSimpleName());
+            assertEquals("value can't be negative", e.getMessage());
+        }
+
+        intrptr = new Interpreter(getClass().getResource("/L2/errors/incrementError256.bf").getFile());
+        try {
+            intrptr.interpretFile();
+            fail();
+        } catch (Exception e) {
+            assertEquals("MemoryOverflowException", e.getClass().getSimpleName());
+            assertEquals("value can't be higher than 255", e.getMessage());
+        }
+
+        intrptr = new Interpreter(getClass().getResource("/L2/errors/left_outMin.bf").getFile());
+        try {
+            intrptr.interpretFile();
+            fail();
+        } catch (Exception e) {
+            assertEquals("PointerPositionOutOfBoundsException", e.getClass().getSimpleName());
+            assertEquals("pointer can't be moved to the left (already at position 0)", e.getMessage());
+        }
+
+
+        intrptr = new Interpreter(getClass().getResource("/L2/errors/right_outMax.bf").getFile());
+        try {
+            intrptr.interpretFile();
+            fail();
+        } catch (Exception e) {
+            assertEquals("PointerPositionOutOfBoundsException", e.getClass().getSimpleName());
+            assertEquals("pointer can't be moved to the right (already at position 29999)", e.getMessage());
+        }
+
     }
+
 
     @Test
     public void interpretBmpFiles() throws Exception {
@@ -229,17 +326,18 @@ public class InterpreterTest {
 
     @Test
     public void rewriteBfFile() throws Exception {
-
+        //TODO
     }
 
     @Test
     public void rewriteBmpFile() throws Exception {
-
+        //TODO
     }
 
 
     @Test
     public void checkWellFormed() throws Exception {
+        //TODO
         //Check tests :
         //Bien formé :
         try {
@@ -251,29 +349,50 @@ public class InterpreterTest {
     }
 
     @Test
-    public void checkBadFormed() throws Exception {
+    public void checkMalFormed() throws Exception {
         //Mal formé :
         try {
-            intrptr = new Interpreter("./examples/L2/JUMP5.bf");
+            intrptr = new Interpreter(getClass().getResource("/L2/errors/malformed/jumpNoBack.bf").getFile());
             intrptr.check();
         } catch (Exception e) {
             assertEquals("BadLoopException", e.getClass().getSimpleName());
             assertEquals("Loop without end : Missing BACK operator", e.getMessage());
         }
         try {
-            intrptr = new Interpreter("./examples/L2/JUMPBACK6.bf");
+            intrptr = new Interpreter(getClass().getResource("/L2/errors/malformed/jumpBackAlone.bf").getFile());
             intrptr.check();
         } catch (Exception e) {
             assertEquals("BadLoopException", e.getClass().getSimpleName());
             assertEquals("Loop without start : Missing JUMP operator", e.getMessage());
         }
         try {
-            intrptr = new Interpreter("./examples/L2/BADPROG.bf");
+            intrptr = new Interpreter(getClass().getResource("/L2/errors/malformed/BADPROG.bf").getFile());
             intrptr.check();
         } catch (Exception e) {
             assertEquals("SyntaxErrorException", e.getClass().getSimpleName());
             assertEquals("Incorrect word operator", e.getMessage());
         }
 
+
     }
+
+    /**************
+     ** LEVEL 3 **
+     **************/
+
+    @Test
+    public void interpretBfFilesWithComments() throws Exception {
+        //Comments
+        intrptr = new Interpreter("./examples/L3/prog_com.bf");
+        intrptr.interpretFile();
+        assertEquals(0, intrptr.getMemory().getP());
+        assertEquals(1, intrptr.getMemory().getCells()[0] & mask);
+
+        intrptr = new Interpreter("./examples/L3/prog_com&indente.bf");
+        intrptr.interpretFile();
+
+        intrptr = new Interpreter("./examples/L3/prog_fullcom.bf");
+        intrptr.interpretFile();
+    }
+
 }
