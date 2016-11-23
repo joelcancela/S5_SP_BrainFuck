3211315b95fb5d09405ac7cda1549b823d7be03f
Pierre Rainero <pierre.rainero@etu.unice.fr>
2016-11-23 08:52:59 +0100

    add example in demo1.sh

--- demo2.sh
+++ demo2.sh
@@ -5,7 +5,6 @@ echo
 read
 echo ./bfck -p src/test/resources/L2/usual/jumpInternalLoop2.bf --translate
 ./bfck -p src/test/resources/L2/usual/jumpInternalLoop2.bf --translate
-start outputTranslate.bmp
 
 read
 clear
@@ -38,6 +37,18 @@ echo ./bfck -p examples/L2/JUMP2.bf --trace
 read
 echo cat examples/L2/JUMP2.log
 cat examples/L2/JUMP2.log
+
+read
+clear
+echo cat examples/L2/JUMPBACK6.bf
+cat examples/L2/JUMPBACK6.bf
+echo
+read
+echo ./bfck -p examples/L2/JUMPBACK6.bf --trace
+./bfck -p examples/L2/JUMPBACK6.bf --trace
+read
+echo cat examples/L2/JUMPBACK6.log
+cat examples/L2/JUMPBACK6.log
 
 read
 clear
