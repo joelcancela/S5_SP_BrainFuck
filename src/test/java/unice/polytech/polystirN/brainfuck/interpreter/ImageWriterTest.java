5a2c1c547d19ad81fbf0f52457aff49611a2a2ac
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2017-01-08 16:50:39 +0100

    ImageWriter now prints in stdout, resolves #16

--- src/test/java/unice/polytech/polystirN/brainfuck/interpreter/ImageWriterTest.java
+++ src/test/java/unice/polytech/polystirN/brainfuck/interpreter/ImageWriterTest.java
@@ -1,23 +1,42 @@
 package unice.polytech.polystirN.brainfuck.interpreter;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+
 import java.io.File;
+import java.io.IOException;
 
 /**
- * Created by Joel CANCELA VAZ on 08/01/2017.
+ * Tests for the ImageWriter class
+ *
+ * @author Joël CANCELA VAZ and Pierre RAINERO
+ * @author Tanguy INVERNIZZI and Aghiles DZIRI
  */
+@Ignore
 public class ImageWriterTest {
+	ImageWriter writer;
 	File file = new File(".log");
 
+	private boolean compareTwoFilesContent(String f1, String f2) throws IOException {
+		File file1 = new File(f1);
+		File file2 = new File(f2);
+		return FileUtils.contentEqualsIgnoreEOL(file1, file2, null);
+	}
 
-	public void writeL1Programs(){
+	@Test
+	public void writeL1Programs() {
+
 
 	}
 
-	public void writeL2Programs(){
-
+	@Test
+	public void writeL2Programs() throws Exception {
+		writer = new ImageWriter(getClass().getResource("/images/helloworld.bmp").getFile());
 	}
 
-	public void writeBadPrograms(){
+	@Test
+	public void writeBadPrograms() {
 
 	}
 }
